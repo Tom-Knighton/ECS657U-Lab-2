30a855a95cce54f3465d251d7372410f6be0e3ee
Tom K <tomknighton@icloud.com>
2023-10-06 09:59:56 +0100

    Stage 3

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,4 +1,5 @@
 using System;
+using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -6,16 +7,20 @@ public class PlayerController : MonoBehaviour
 {
     public Vector2 moveValue;
     public float speed;
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI winText;
 
     private Rigidbody _rigidbody;
     private int _count;
 
+    private const int TotalPickUps = 9;
     private const string PickUpTag = "PickUp";
 
     private void Start()
     {
         _rigidbody = GetComponent<Rigidbody>();
         _count = 0;
+        winText.text = string.Empty;
     }
 
     void OnMove(InputValue val)
@@ -23,13 +28,14 @@ public class PlayerController : MonoBehaviour
         moveValue = val.Get<Vector2>();
     }
 
+
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag(PickUpTag))
-        {
-            other.gameObject.SetActive(false);
-            _count += 1;
-        }
+        if (!other.gameObject.CompareTag(PickUpTag)) return;
+        
+        other.gameObject.SetActive(false);
+        _count += 1;
+        SetCountText();
     }
 
     private void FixedUpdate()
@@ -37,5 +43,14 @@ public class PlayerController : MonoBehaviour
         var movement = new Vector3(moveValue.x, 0, moveValue.y);
         
        _rigidbody.AddForce(movement * (speed * Time.fixedDeltaTime));
+    }
+
+    private void SetCountText()
+    {
+        scoreText.text = $"Score: {_count}";
+        if (_count >= TotalPickUps)
+        {
+            winText.text = "You Win!";
+        }
     }
 }
