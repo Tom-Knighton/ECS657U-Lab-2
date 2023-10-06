5d07fcd0905f041a09eaf12b3b23ff910cd16ed0
Tom K <tomknighton@icloud.com>
2023-10-06 09:48:02 +0100

    Stage 2

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -7,15 +8,28 @@ public class PlayerController : MonoBehaviour
     public float speed;
 
     private Rigidbody _rigidbody;
+    private int _count;
+
+    private const string PickUpTag = "PickUp";
 
     private void Start()
     {
         _rigidbody = GetComponent<Rigidbody>();
+        _count = 0;
     }
 
     void OnMove(InputValue val)
     {
         moveValue = val.Get<Vector2>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag(PickUpTag))
+        {
+            other.gameObject.SetActive(false);
+            _count += 1;
+        }
     }
 
     private void FixedUpdate()
